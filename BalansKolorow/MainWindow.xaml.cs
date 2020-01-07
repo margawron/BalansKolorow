d63ef15aea291ae7b0c198a0fdb597e78682352e
margawron <max2.marcel@gmail.com>
2020-01-07 16:25:32 +0100

    Zaimplementowano algorytm balansu kolorów w C++. Zarówno dla dodawania jak i dla mnożenia.

--- BalansKolorow/MainWindow.xaml.cs
+++ BalansKolorow/MainWindow.xaml.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Security;
+using System.Diagnostics;
 
+
+
+// Pomysł: Użyć OpenGL żeby renderować postęp na żywo
 namespace BalansKolorow
 {
     /// <summary>
@@ -21,35 +19,230 @@ namespace BalansKolorow
     /// </summary>
     public partial class MainWindow : Window
     {
-        [DllImport("ja_cpp.dll")]
-        public extern static IntPtr FileCPP();
+        // Uchwyt do kontrolki z widokiem na obraz
+        System.Windows.Controls.Image imageView;
+        // Uchwyt do kontrolki z wyborem ilości wątków
+        System.Windows.Controls.Slider sliderThreads;
+        // Uchwyt do kontrolki ze ścieżka do bitmapy
+        System.Windows.Controls.TextBox bitmapPathTextBox;
 
-        [DllImport("ja_asm.dll",CallingConvention = CallingConvention.StdCall, EntryPoint = "FileASM")]
+        // Ścieżka do pliku
+        string bitmapFilename;
+        // Wskaźnik do tablicy bajtów bitmapy
+        byte[] bitmapByteArray;
+        // szerokość bitmapy
+        int width;
+        // wysokość bitmapy
+        int height;
+        // stoper
+        Stopwatch stopwatch;
+    
+
+        [DllImport("ja_cpp.dll", EntryPoint = "AdditionBitmapColorBalancer")]
+        public extern static unsafe void CPPAdditionBalance(byte* bitmap, int size, byte* argb);
+
+        [DllImport("ja_cpp.dll", EntryPoint = "MultiplicationColorBalancer")]
+        public extern static unsafe void CPPMultiplicationBalance(byte* bitmap, int size, float* argb);
+
+        [DllImport("ja_asm.dll", CallingConvention = CallingConvention.StdCall, EntryPoint = "FileASM")]
         public extern static IntPtr FileASM();
 
+        // 
         public MainWindow()
         {
             InitializeComponent();
+            imageView = (System.Windows.Controls.Image)this.FindName("ImageView");
+            sliderThreads = (System.Windows.Controls.Slider)this.FindName("SliderAmountOfThreads");
+            bitmapPathTextBox = (System.Windows.Controls.TextBox)this.FindName("BitmapUriTextBox");
+        }
+        // Obsługa przycisku "Wybierz bitmapę"
+        private void LoadBitmap(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Bitmap|*.bmp";
+            openFileDialog.FilterIndex = 1;
+            if(openFileDialog.ShowDialog() == true)
+            {
+                // Odbierz nazwę pliku
+                bitmapFilename = openFileDialog.FileName;
+                // Ustaw ścieżkę pliku w polu tekstowym
+                bitmapPathTextBox.Text = bitmapFilename;
+                // Wczytaj bitmapę z pliku i przekonwertuj na BGRA32
+                var bitmap = ConvertBitmapToBgra32(new Bitmap(bitmapFilename));
+                // Wyświetl bitmapę w kontrolce
+                imageView.Source = Convert(bitmap);
+                // Przekonwertuj bitmapę do tablicy bajtów
+                bitmapByteArray = ImageToBytes(bitmap);
+            }
         }
 
-        private void LaunchHelloWorld(object sender, RoutedEventArgs e)
+        private void SaveBitmap(Bitmap bitmap)
         {
-            var elo = (TextBox)this.FindName("textbox");
-            elo.Text = "Hello World";
+            bitmap.Save("save.bmp");
         }
 
-        private void LauchASM(object sender, RoutedEventArgs e)
+        // Zamienia format bitmapy na BGRA(4 bajtowy)
+        public Bitmap ConvertBitmapToBgra32(Bitmap bitmap)
         {
-            var elo = (TextBox)this.FindName("textbox");
-            IntPtr lel = FileASM();
-            elo.Text = Marshal.PtrToStringAnsi(lel);
+            // Sprawdz czy obraz nie jest już w dobrym formacie
+            if (bitmap.PixelFormat == System.Drawing.Imaging.PixelFormat.Format32bppArgb)
+            {
+                return bitmap; // Jest Ok, nic nie zmieniaj
+            }
+            else
+            {
+                // Utwórz bitmapę wyjściową w poprawnym formacie
+                Bitmap reformattedBitmap = new Bitmap(bitmap.Width, bitmap.Height,
+                    System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+                // Przekonwetuj na grafikę
+                using (System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(reformattedBitmap))
+                {
+                    // Zapisz na nowo obraz
+                    graphics.DrawImage(bitmap, new Rectangle(0, 0, reformattedBitmap.Width, reformattedBitmap.Height));
+                }
+
+                // Zwróć przeformatowany obraz
+                return reformattedBitmap;
+            }
+        }
+        // Zczytuje bitmapę i zapisuje jako tablicę bajtów
+        // Zwraca wskaźnik na tą tablicę
+        public byte[] ImageToBytes(Bitmap bitmap)
+        {
+            byte[] imageBytes;
+            // Pobierz szerokość bitmapy
+            width = bitmap.Width;
+            // Pobierz wysokość bitmapy
+            height = bitmap.Height;
+            // Utwórz strukturę mówiącą jak wyskalować bitmapę
+            // W tym przypadku nie skaluj
+            var rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            // "Przytrzymaj" bitmapę w pamięci w trybie R/W oraz w formacie BGRA (little endian)
+            BitmapData bitmapInArgb = bitmap.LockBits(rect,ImageLockMode.ReadWrite,PixelFormat.Format32bppArgb);
+            // Wielkość tablicy w pixelach * rozmiar pixela
+            int sizeOfTheArray = width * height * 4;
+            // Alokacja pamięci
+            imageBytes = new byte[sizeOfTheArray];
+            // Skopiuj bitmapę do tablicy bajtów
+            Marshal.Copy(bitmapInArgb.Scan0, imageBytes, 0 , sizeOfTheArray);
+            // Uwolnij bitmapę
+            bitmap.UnlockBits(bitmapInArgb);
+
+            return imageBytes;
         }
 
-        private void LaunchCPP(object sender, RoutedEventArgs e)
+        public Bitmap BytesToBitmap(byte[] bytes)
         {
-            var elo = (TextBox)this.FindName("textbox");
-            elo.Text = Marshal.PtrToStringAnsi(FileCPP());
+            Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            var rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            BitmapData bitmapInArgb = bitmap.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            Marshal.Copy(bitmapByteArray, 0, bitmapInArgb.Scan0, bitmapByteArray.Length);
+            bitmap.UnlockBits(bitmapInArgb);
+
+            return bitmap;
         }
+
+        // Zamień bitmapę na źródło obrazu dla kontrolki
+        public BitmapImage Convert(Bitmap src)
+        {
+            // Utwórz strumień pamięci
+            MemoryStream ms = new MemoryStream();
+            // Zapisz bitmapę do strumienia pamięci
+            src.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            // Utwórz nowe źródło obrazu dla konrolki
+            BitmapImage image = new BitmapImage();
+            // Rozpocznij inicjalizację obrazu
+            image.BeginInit();
+            // Każ strumieniowi zaczynać od początku bitmapy
+            ms.Seek(0, SeekOrigin.Begin);
+            // Przypisz strumień do bitmapy
+            image.StreamSource = ms;
+            // Zakończ inicjalizację obrazu
+            image.EndInit();
+            // Zwróć źródło
+            return image;
+        }
+
+        // TODO
+        private void goVisual() { Console.WriteLine("TODO"); }
+
+
+        private void DebugInfo()
+        {
+            Console.WriteLine("Byte array has size of {0}", bitmapByteArray.Length);
+            Console.WriteLine($"width: {width}, bitmapHeight: {height}");
+            for (int i = 0; i < (bitmapByteArray.Length < 64 ? bitmapByteArray.Length : 64); i += 4)
+            {
+                Console.WriteLine($"Bytes {bitmapByteArray[i]}, {bitmapByteArray[i + 1]}, {bitmapByteArray[i + 2]}, {bitmapByteArray[i+3]}" );
+            }
+        }
+
+        private unsafe void RunCPP(object sender, RoutedEventArgs e)
+        {
+            CPPMultiplication();
+        }
+
+        private unsafe void CPPAddition()
+        {
+            stopwatch = new Stopwatch();
+            byte[] argb = new byte[4];
+            argb[0] = 0;  // Alpha
+            argb[1] = 50; // Red
+            argb[2] = 0;  // Green
+            argb[3] = 0;  // Blue
+            fixed (byte* bitmapPointer = bitmapByteArray, argbPointer = argb)
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+                // TODO dodać wielowątkowość
+                CPPAdditionBalance(bitmapPointer, bitmapByteArray.Length, argbPointer);
+                stopwatch.Stop();
+            }
+            Console.WriteLine("Operation took: {0} ms", stopwatch.ElapsedMilliseconds);
+            var bitmap = BytesToBitmap(bitmapByteArray);
+            SaveBitmap(bitmap);
+        }
+
+        private unsafe void CPPMultiplication()
+        {
+            stopwatch = new Stopwatch();
+            // TODO pobrać kolory
+            float[] argb = new float[4];
+            argb[0] = 1f;  // Alpha
+            argb[1] = 1f; // Red
+            argb[2] = 1f;  // Green
+            argb[3] = 3f;  // Blue
+            fixed (byte* bitmapPointer = bitmapByteArray)
+            {
+                fixed (float* argbPointer = argb)
+                {
+                    stopwatch.Reset();
+                    stopwatch.Start();
+                    // TODO dodać wielowątkowość
+                    CPPMultiplicationBalance(bitmapPointer, bitmapByteArray.Length, argbPointer);
+                    stopwatch.Stop();
+                }
+            }
+            Console.WriteLine("Operation took: {0} ms", stopwatch.ElapsedMilliseconds);
+            var bitmap = BytesToBitmap(bitmapByteArray);
+            SaveBitmap(bitmap);
+        }
+
     }
 }
 
+/**
+ * public void TestMethod()
+{
+    var incoming = new byte[100];
+    fixed (byte* inBuf = incoming)
+    {
+        byte* outBuf = bufferOperations(inBuf, incoming.Length);
+        // Assume, that the same buffer is returned, only with data changed.
+        // Or by any other means, get the real lenght of output buffer (e.g. from library docs, etc).
+        for (int i = 0; i < incoming.Length ; i++)
+           incoming[i] = outBuf[i];
+    }
+}
+*/
--- ja_cpp/ja_cpp.cpp
+++ ja_cpp/ja_cpp.cpp
@@ -4,7 +4,44 @@
 const char* hello = "Hello Cpp";
 
 extern "C" {
+	// Funkcja testowa zwracaj¹ca zmienn¹ hello
 	__declspec(dllexport) char* FileCPP() {
 		return const_cast<char *>(hello);
+	}
+
+	// http://locklessinc.com/articles/sat_arithmetic/
+
+
+	__declspec(dllexport) void AdditionBitmapColorBalancer(unsigned char* bitmap, int size,unsigned char* argb) {
+		// Dodaj z uwzglêdnieniem saturacji unikamy w ten sposób usterek w obrazie
+		// Lambda pozwoli u³atwiæ kompilatorowi optymalizacjê
+		auto saturateColors = [](unsigned char orignalComponent,unsigned char raiseComponentBy) {
+			unsigned char result = orignalComponent + raiseComponentBy;
+			return (result |= (-result < orignalComponent));
+		};
+		// Little endian sprawia ¿e bitmapa jest w formacie bgra natomiast argb nadal jest w 
+		for (int i = 0; i < size; i += 4) {
+			bitmap[i] = saturateColors(bitmap[i], argb[3]);		// blue
+			bitmap[i + 1] = saturateColors(bitmap[i + 1], argb[2]);	// green
+			bitmap[i + 2] = saturateColors(bitmap[i + 2], argb[1]);	// red
+			bitmap[i + 3] = saturateColors(bitmap[i + 3], argb[0]);	// alpha
+		}
+	}
+
+	__declspec(dllexport) void MultiplicationColorBalancer(unsigned char* bitmap, int size, float* argb) {
+		// Little endian sprawia ¿e bitmapa jest w formacie bgra
+		auto saturateColors = [](unsigned char originalComponent, float raiseComponentBy) {
+			unsigned short result = (float)originalComponent * raiseComponentBy;
+			unsigned char highByte = result >> 8;
+			unsigned char lowByte = (unsigned char) result;
+			unsigned char returnVal = lowByte | -!!highByte;
+			return returnVal;
+		};
+		for (int i = 0; i < size; i += 4) {
+			bitmap[i] = saturateColors(bitmap[i], argb[3]);
+			bitmap[i + 1] = saturateColors(bitmap[i + 1], argb[2]);
+			bitmap[i + 2] = saturateColors(bitmap[i + 2], argb[1]);
+			bitmap[i + 3] = saturateColors(bitmap[i + 3], argb[0]);
+		}
 	}
 }
